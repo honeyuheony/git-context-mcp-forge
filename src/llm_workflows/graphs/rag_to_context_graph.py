dc19e7a87a14d729f9fc6505b7ec01460f1270d8
Kuhy <kwh1019_@naver.com>
2025-04-21 15:50:31 +0900

    feat: RAG to Context 그래프에 문서 포맷팅 노드 추가

--- src/llm_workflows/graphs/rag_to_context_graph.py
+++ src/llm_workflows/graphs/rag_to_context_graph.py
@@ -2,6 +2,7 @@ from langgraph.graph import END, StateGraph, START
 from langgraph.graph.state import CompiledStateGraph
 from src.llm_workflows.state import RagToContextState
 from src.llm_workflows.nodes.retriever import search_documents
+from src.llm_workflows.nodes.document_formatting import format_documents
 from src.config.log_config import Logger
 
 logger = Logger()
@@ -10,8 +11,10 @@ def create_rag_to_context_graph() -> CompiledStateGraph:
     workflow = StateGraph(RagToContextState)
     
     workflow.add_node("검색", search_documents)
+    workflow.add_node("포맷팅", format_documents)
     
     workflow.add_edge(START, "검색")
-    workflow.add_edge("검색", END)
+    workflow.add_edge("검색", "포맷팅")
+    workflow.add_edge("포맷팅", END)
     
     return workflow.compile()
